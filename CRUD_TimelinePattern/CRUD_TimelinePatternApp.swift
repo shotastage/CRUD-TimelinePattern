3bbd7daad902966ccddaede052a178232a5567e5
shotastage <hornet.live.mf@gmail.com>
2022-11-23 23:47:15 +0900

    Initial Commit

---
+++ CRUD_TimelinePattern/CRUD_TimelinePatternApp.swift
@@ -0,0 +1,17 @@
+//
+//  CRUD_TimelinePatternApp.swift
+//  CRUD_TimelinePattern
+//
+//  Created by Shota Shimazu on 2022/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CRUD_TimelinePatternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CRUD_TimelinePattern/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CRUD_TimelinePattern
+//
+//  Created by Shota Shimazu on 2022/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
